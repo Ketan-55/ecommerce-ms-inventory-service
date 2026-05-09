55a02c989935e224acc908e1240aefe30f7ba19e
Ketan Behere <Ketanbehere555@gmail.com>
2026-05-09 17:45:05 +0530

    feat(inventory-service): publish inventory success and failure events after stock processing

--- src/main/java/com/ecommerce/inventory_service/kafka/KafkaConsumer.java
+++ src/main/java/com/ecommerce/inventory_service/kafka/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.ecommerce.inventory_service.kafka;
 
+import com.ecommerce.inventory_service.dto.InventoryEvent;
 import com.ecommerce.inventory_service.dto.OrderEvent;
 import com.ecommerce.inventory_service.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,18 +13,41 @@ public class KafkaConsumer {
     @Autowired
     private InventoryService inventoryService;
 
+    @Autowired
+    KafkaProducer inventoryProducer;
+
     @KafkaListener(topics = "order-topic", groupId = "inventory-group")
     public void consume(OrderEvent event){
 
-        System.out.println("🔥 Received order event: " + event.getOrderId());
+        System.out.println("📦 Received order event for product: "
+                + event.getProductId());
+
+        InventoryEvent inventoryEvent = new InventoryEvent();
+
+        inventoryEvent.setOrderId(event.getOrderId());
+        inventoryEvent.setProductId(event.getProductId());
 
         try {
-            inventoryService.reduceStock(event.getProductId(), event.getQuantity());
-            System.out.println("✅ Stock reduced for product: " + event.getProductId());
-        } catch (RuntimeException e) {
-            System.err.println("❌ Failed to reduce stock for product: " + event.getProductId() + " - " + e.getMessage());
+
+            inventoryService.reduceStock(
+                    event.getProductId(),
+                    event.getQuantity()
+            );
+
+            inventoryEvent.setStatus("SUCCESS");
+            inventoryEvent.setMessage("Stock updated successfully");
+
+            System.out.println("✅ Inventory updated");
+
+        } catch (Exception e) {
+
+            inventoryEvent.setStatus("FAILED");
+            inventoryEvent.setMessage(e.getMessage());
+
+            System.out.println("❌ Inventory update failed");
         }
 
+        inventoryProducer.publishInventoryEvent(inventoryEvent);
     }
 
 
--- src/main/java/com/ecommerce/inventory_service/kafka/KafkaProducer.java
+++ src/main/java/com/ecommerce/inventory_service/kafka/KafkaProducer.java
@@ -3,7 +3,9 @@ package com.ecommerce.inventory_service.kafka;
 import com.ecommerce.inventory_service.dto.InventoryEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KafkaProducer {
 
     @Autowired
