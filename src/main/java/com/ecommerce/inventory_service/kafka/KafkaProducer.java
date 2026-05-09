8233159075ded9f8e16842f15100db58ee4362d9
Ketan Behere <Ketanbehere555@gmail.com>
2026-05-09 17:08:18 +0530

    feat(inventory-service): add Kafka producer for publishing inventory status events

--- src/main/java/com/ecommerce/inventory_service/kafka/KafkaProducer.java
+++ src/main/java/com/ecommerce/inventory_service/kafka/KafkaProducer.java
@@ -1,4 +1,18 @@
 package com.ecommerce.inventory_service.kafka;
 
+import com.ecommerce.inventory_service.dto.InventoryEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
 public class KafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void publishInventoryEvent(InventoryEvent event) {
+
+        kafkaTemplate.send("inventory-topic", event);
+
+        System.out.println("📤 Inventory event published");
+    }
 }
